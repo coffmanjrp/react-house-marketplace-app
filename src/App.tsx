bb69899812b59a6225786827935275c0fdee9858
coffmanjrp <coffmanjrp@gmail.com>
2021-12-13 22:07:03 -0800

    Create Navbar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from 'components';
 import {
   Explore,
   ForgotPassword,
@@ -14,12 +15,13 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Explore />} />
-          <Route path="/offers" element={<Offers />} />
+          <Route path="/offer" element={<Offers />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
     </>
   );
