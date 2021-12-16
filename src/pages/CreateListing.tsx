ee16087a64c0e3046a3dfe1908b1dc6959368d00
coffmanjrp <coffmanjrp@gmail.com>
2021-12-16 15:27:42 -0800

    Implement Uploading Images to Firebase and Save Listings to Firestore

--- src/pages/CreateListing.tsx
+++ src/pages/CreateListing.tsx
@@ -8,8 +8,16 @@ import {
   useState,
 } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { app } from 'firebase.config';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import { app, db } from 'firebase.config';
 import { Spinner } from 'components';
 import { toast } from 'react-toastify';
 
@@ -20,11 +28,12 @@ interface FormDataState {
   bathrooms: number;
   parking: boolean;
   furnished: boolean;
-  address: string;
+  address?: string;
   offer: boolean;
   regularPrice: number;
-  discountedPrice: number;
-  images: FileList | [] | null;
+  discountedPrice?: number;
+  images?: any;
+  location?: string;
   latitude: number;
   longitude: number;
   userRef?: string;
@@ -44,7 +53,7 @@ const CreateListing: FC = () => {
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
-    images: [],
+    images: {},
     latitude: 0,
     longitude: 0,
   });
@@ -91,7 +100,7 @@ const CreateListing: FC = () => {
 
     setLoading(true);
 
-    if (discountedPrice >= regularPrice) {
+    if (discountedPrice! >= regularPrice) {
       setLoading(false);
       toast.error('Disoundted price needs to be less than regular price');
       return;
@@ -115,8 +124,8 @@ const CreateListing: FC = () => {
       );
       const data = await responce.json();
 
-      geolocation.lat = data.results[0]?.geometry.lat ?? 0;
-      geolocation.lng = data.results[0]?.geometry.lng ?? 0;
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
       location =
         data.status === 'ZERO_RESULTS'
           ? undefined
@@ -130,10 +139,69 @@ const CreateListing: FC = () => {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
+    // Store image in firebase
+    const storeImage = async (image: File) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser!.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, 'images/' + fileName);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imageUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false);
+      toast.error('Images not uploaded');
+      return;
+    });
+
+    const formDataCopy = {
+      ...formData,
+      imageUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
+    formDataCopy.location = address;
+    delete formDataCopy.images;
+    delete formDataCopy.address;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+    const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
+
     setLoading(false);
+    toast.success('Listing saved');
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
   };
 
   const onMutate = (e: ChangeEvent | MouseEvent) => {
